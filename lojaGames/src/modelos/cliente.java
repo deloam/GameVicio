977eafd2f75ed546d8adbceef2d0b0050670707b
deloam <deyvisondn@hotmail.com>
2015-10-18 12:03:00 -0200

    Criacao dos contrutores

--- lojaGames/src/modelos/cliente.java
+++ lojaGames/src/modelos/cliente.java
@@ -14,11 +14,50 @@ import sun.util.calendar.BaseCalendar.Date;
  * @author Deloam Kitty
  */
 public class cliente {
-    String nomeCliente;
-    Date dataNascCliente;
+    long id;
+    String nome;
+    Date dataNasc;
+    String cpf;
     
     sexo sexo;
     
     endereco endereco;
+    
+    contato contato;
+
+    public cliente(long id, String nome, Date dataNasc, String cpf, sexo sexo, endereco endereco, contato contato) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.contato = contato;
+    }
+
+    public cliente(long id, String nome, Date dataNasc, String cpf, sexo sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.cpf = cpf;
+        this.sexo = sexo;
+    }
+
+    public cliente(String nome, Date dataNasc, String cpf, sexo sexo, endereco endereco, contato contato) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.endereco = endereco;
+        this.contato = contato;
+    }
+
+    public cliente(String nome, Date dataNasc, sexo sexo) {
+        this.nome = nome;
+        this.dataNasc = dataNasc;
+        this.sexo = sexo;
+    }
+    
     
 }
